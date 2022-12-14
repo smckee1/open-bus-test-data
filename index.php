69b8dd6920adbf9c7698cb79b21f56ed18da5166
smckee1 <59446921+smckee1@users.noreply.github.com>
2022-12-14 14:07:13 +0000

    Changes made for the netlify app

---
+++ index.php
@@ -0,0 +1,287 @@
+<?php include_once("home.html")	
+?> 
+
+<?php
+echo "<p>Timetable LARGE 66mb zip</p>
+";
+$path = "/app/testdata/Timetable LARGE 66mb zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetable LARGE 66mb zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables Not Valid xml</p>
+";
+$path = "/app/testdata/Timetables Not Valid xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables Not Valid xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables Not Valid zip</p>
+";
+$path = "/app/testdata/Timetables Not Valid zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables Not Valid zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI FAIL DQ FAIL xml</p>
+";
+$path = "/app/testdata/Timetables PTI FAIL DQ FAIL xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI FAIL DQ FAIL xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI FAIL DQ FAIL zip</p>
+";
+$path = "/app/testdata/Timetables PTI FAIL DQ FAIL zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI FAIL DQ FAIL zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI FAIL DQ PASS xml</p>
+";
+$path = "/app/testdata/Timetables PTI FAIL DQ PASS xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI FAIL DQ PASS xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI FAIL DQ PASS zip</p>
+";
+$path = "/app/testdata/Timetables PTI FAIL DQ PASS zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI FAIL DQ PASS zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI PASS DQ FAIL xml</p>
+";
+$path = "/app/testdata/Timetables PTI PASS DQ FAIL xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI PASS DQ FAIL xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI PASS DQ FAIL zip</p>
+";
+$path = "/app/testdata/Timetables PTI PASS DQ FAIL zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI PASS DQ FAIL zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI PASS DQ PASS xml</p>
+";
+$path = "/app/testdata/Timetables PTI PASS DQ PASS xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI PASS DQ PASS xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Timetables PTI PASS DQ PASS zip</p>
+";
+$path = "/app/testdata/Timetables PTI PASS DQ PASS zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Timetables PTI PASS DQ PASS zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>AVL Siri SM</p>
+";
+$path = "/app/testdata/AVL Siri SM";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/AVL Siri SM/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>AVL Siri VM request</p>
+";
+$path = "/app/testdata/AVL Siri VM request";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/AVL Siri VM request/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>AVL Siri VM response</p>
+";
+$path = "/app/testdata/AVL Siri VM response";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/AVL Siri VM response/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>AVL Siri VM request simple</p>
+";
+$path = "/app/testdata/AVL Siri VM request simple";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/AVL Siri VM request simple/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>AVL Siri VM response simple</p>
+";
+$path = "/app/testdata/AVL Siri VM response simple";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/AVL Siri VM response simple/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Fares xml</p>
+";
+$path = "/app/testdata/Fares xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Fares xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Fares second xml</p>
+";
+$path = "/app/testdata/Fares second xml";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Fares second xml/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
+
+<?php
+echo "<p>Fares zip</p>
+";
+$path = "/app/testdata/Fares zip";
+$dh = opendir($path);
+$i=1;
+while (($file = readdir($dh)) !== false) {
+    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+        echo "<a href='/testdata/Fares zip/$file'>$file</a><br /><br />";
+        $i++;
+    }
+}
+closedir($dh);
+?> 
